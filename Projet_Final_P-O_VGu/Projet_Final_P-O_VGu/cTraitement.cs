5db05d369b842fca4023ef71ed73656d374e861d
Vyktors <guerinmathieu19@gmail.com>
2017-12-02 19:02:48 -0500

    fix l'attribution du nombre de vaisseaux

--- Projet_Final_P-O_VGu/Projet_Final_P-O_VGu/cTraitement.cs
+++ Projet_Final_P-O_VGu/Projet_Final_P-O_VGu/cTraitement.cs
@@ -10,20 +10,29 @@ namespace Projet_Final_P_O_VGu
 
     class cTraitement
     {
-        int nbVaisseauLeger, nbVaisseauCargo;
-
+        int nbVaisseau, nbVaisseauLeger, nbVaisseauCargo, ratioLegerCargo;
+        Random nbRandom;
         public cTraitement()
         {
-
+            nbRandom = new Random();
         }
 
         /*L'utilisateur décide du nombre de vaisseau léger et cargo*/
         public void selectionnerNbVaisseau()
         {
-            Console.Write("Entrez le nombre de vaisseaux légers désiré (Le ratio vaisseau leger/ vaisseau cargo est de 1:1): ");
-            nbVaisseauLeger = Convert.ToInt32(Console.ReadLine())/2;
-            nbVaisseauCargo = nbVaisseauLeger;
+            Console.Write("Entrez le nombre de vaisseaux légers désiré : ");
+            nbVaisseau = Convert.ToInt32(Console.ReadLine());
 
+            ratioLegerCargo = nbRandom.Next(0, 101);
+            nbVaisseauLeger = nbVaisseau * ratioLegerCargo / 100;
+            nbVaisseauCargo = nbVaisseau - nbVaisseauLeger;
+
+            Console.WriteLine("Le nombre de vaisseaus Legers à été attribué aléatoirement à : " + ratioLegerCargo + "%");
+            Console.WriteLine("     " + nbVaisseauLeger + " Vaisseaux légers");
+            Console.WriteLine("     " + nbVaisseauCargo + " Vaisseaux Cargo");
+
+            Console.WriteLine("Appuyer sur une touche pour attribuer les ressources aux vaisseaux");
+            Console.ReadKey();
             affecterQuantite(nbVaisseauLeger, nbVaisseauCargo);
 
         }
@@ -31,10 +40,10 @@ namespace Projet_Final_P_O_VGu
         /*Créer tous les vaisseaux de départ , et affecte de façon aléatoire, la quantité de chaque matière pour chaque vaisseau*/
         private void affecterQuantite(int nombreVaissLeger, int nombreVaissCargo)
         {
-            Random rndQtMatiere;
+            
             int qtPlutonium, qtUranium, qtML, qtTerre, qtRes, qtVaisseau;
 
-            rndQtMatiere = new Random();
+            
 
             qtPlutonium = 0;
             qtUranium = 0;
@@ -46,23 +55,23 @@ namespace Projet_Final_P_O_VGu
             {
                 qtVaisseau = 108;
 
-                qtPlutonium = rndQtMatiere.Next(1, qtVaisseau - 4);
+                qtPlutonium = nbRandom.Next(1, qtVaisseau - 4);
                 qtVaisseau -= qtPlutonium;
 
 
-                qtUranium = rndQtMatiere.Next(1, qtVaisseau - 3);
+                qtUranium = nbRandom.Next(1, qtVaisseau - 3);
                 qtVaisseau -= qtUranium;
 
 
-                qtML = rndQtMatiere.Next(1, qtVaisseau - 2);
+                qtML = nbRandom.Next(1, qtVaisseau - 2);
                 qtVaisseau -= qtML;
 
 
-                qtTerre = rndQtMatiere.Next(1, qtVaisseau - 1);
+                qtTerre = nbRandom.Next(1, qtVaisseau - 1);
                 qtVaisseau -= qtTerre;
 
 
-                qtRes = rndQtMatiere.Next(1, qtVaisseau);
+                qtRes = nbRandom.Next(1, qtVaisseau);
                 qtVaisseau -= qtRes;
 
             cVaisseauLeger vaisseauLeger = new cVaisseauLeger(qtPlutonium, qtUranium, qtML, qtTerre, qtRes);
@@ -75,23 +84,23 @@ namespace Projet_Final_P_O_VGu
             {
                 qtVaisseau = 367;
 
-                qtPlutonium = rndQtMatiere.Next(1, qtVaisseau - 4);
+                qtPlutonium = nbRandom.Next(1, qtVaisseau - 4);
                 qtVaisseau -= qtPlutonium;
 
 
-                qtUranium = rndQtMatiere.Next(1, qtVaisseau - 3);
+                qtUranium = nbRandom.Next(1, qtVaisseau - 3);
                 qtVaisseau -= qtUranium;
 
 
-                qtML = rndQtMatiere.Next(1, qtVaisseau - 2);
+                qtML = nbRandom.Next(1, qtVaisseau - 2);
                 qtVaisseau -= qtML;
 
 
-                qtTerre = rndQtMatiere.Next(1, qtVaisseau - 1);
+                qtTerre = nbRandom.Next(1, qtVaisseau - 1);
                 qtVaisseau -= qtTerre;
 
 
-                qtRes = rndQtMatiere.Next(1, qtVaisseau);
+                qtRes = nbRandom.Next(1, qtVaisseau);
                 qtVaisseau -= qtRes;
 
                 cVaisseauCargo vaisseauCargo = new cVaisseauCargo(qtPlutonium, qtUranium, qtML, qtTerre, qtRes);
